91b3ae03428f935019416ebe8b41b2ee8cdb4c6b
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-14 22:41:39 -0400

    successfull connection to db

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 import express  from 'express'
 import cors from 'cors'
+import connectDb from './db.js'
+import 'dotenv/config'
 
 const app= express()
 
-const port = 8080
+const port = process.env.PORT
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.json(" Hello User")
+    res.json(" Hello User") //connect to server
+    
 })
 
-app.listen(port, () => console.log("Listening to port: "  + port))
+app.listen(port, () => {console.log("Listening to port: "  + port),
+    connectDb() //connect to db
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ async function getData() {
 }
 
 getData()
-
   
 }, [])
 
