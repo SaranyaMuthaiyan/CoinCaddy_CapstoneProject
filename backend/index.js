476975c9b482944dea971688f8455d15aef462db
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-17 21:08:07 -0400

    corrections

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ import cors from 'cors'
 import connectDb from './db.js'
 import 'dotenv/config'
 
+import Transaction from '..models/transaction.js'
+import User from '..models/User.js';
+
 const app= express()
 
 const port = process.env.PORT
 
 app.use(cors())
+
+app.get()
 
 app.get('/', (req, res) => {
     res.json(" Hello User") //connect to server
