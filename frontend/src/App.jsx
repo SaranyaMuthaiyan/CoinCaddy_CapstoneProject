f20073cae849b87c6bb36db5edd74d30ee8b4b05
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-17 21:31:18 -0400

    created RegisterPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,27 @@
-import { useState, useEffect } from 'react'
-
+// import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 import './App.css'
 
 function App() {
-
-useEffect(() => {
-async function getData() {
-    try {
-  const response = await fetch('http://localhost:8080')
-  const data = await response.json()
-  console.log(data)
-}
-  catch(e){
-  console.log(e)
-  }
-}
-
-getData()
-  
-}, [])
-
   return (
     <>
-      Hello (from frontend)
+    <nav className="bg-blue-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <a href='#' className="text-white text-lg font-semibold">
+Nuevo Tracker </a>
+         </div>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Login />} />
+       <Route path="/register" element={<Register />} />
+       <Route path="/dashboard" element={<Dashboard />} />
+   </Routes>
     </>
   )
-}
+  }
 
 export default App
+   
