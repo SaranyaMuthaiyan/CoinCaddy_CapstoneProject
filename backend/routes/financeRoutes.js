c638190eac3d02fe7722aa410f4640e57c98c4a4
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-20 23:24:11 -0400

    routefile

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -3,10 +3,7 @@ import express from 'express';
 import Expense from '../models/Expense.js';
 const router = express.Router();
 
-// const incomeData = [
-//   { source: 'Salary', amount: 3000 },
-//   { source: 'Freelance', amount: 1200 }
-// ];
+
 
 // const expenseData = [
 //   { category: 'Rent', amount: 1200, source: 'Salary' },
---
+++ backend/routes/incomeRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import Income from '../models/Income.js';
+const router = express.Router();
+
+
+// const incomeData = [
+//   { source: 'Salary', amount: 3000 },
+//   { source: 'Freelance', amount: 1200 }
+// ];
