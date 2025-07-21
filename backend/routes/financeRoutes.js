ea3cadf2ed83d040d90991d032f1b6f98177672b
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-20 23:21:57 -0400

    changes in routes

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -1,20 +1,56 @@
 // routes/financeRoutes.js
 import express from 'express';
+import Expense from '../models/Expense.js';
 const router = express.Router();
 
-const incomeData = [
-  { source: 'Salary', amount: 3000 },
-  { source: 'Freelance', amount: 1200 }
-];
+// const incomeData = [
+//   { source: 'Salary', amount: 3000 },
+//   { source: 'Freelance', amount: 1200 }
+// ];
 
-const expenseData = [
-  { category: 'Rent', amount: 1200, source: 'Salary' },
-  { category: 'Groceries', amount: 400, source: 'Salary' },
-  { category: 'Utilities', amount: 150, source: 'Freelance' },
-  { category: 'Entertainment', amount: 200, source: 'Freelance' }
-];
+// const expenseData = [
+//   { category: 'Rent', amount: 1200, source: 'Salary' },
+//   { category: 'Groceries', amount: 400, source: 'Salary' },
+//   { category: 'Utilities', amount: 150, source: 'Freelance' },
+//   { category: 'Entertainment', amount: 200, source: 'Freelance' }
+// ];
 
-router.get('/income', (req, res) => res.json(incomeData));
-router.get('/expenses', (req, res) => res.json(expenseData));
+// router.get('/income', (req, res) => res.json(incomeData));
+router.get('/expenses', async (req, res) => {
+  const exp = await Expense.find();
+  res.status(200).json(exp);
+})
+
+router.post('/expenses', async (req, res) => {
+  try {
+    const { category, amount, source } = req.body;
+    const newExpense = new Expense({ category, amount, source, createdAt: Date.now() });
+    await newExpense.save();
+    res.status(201).json(newExpense);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.put('/expenses/:id', async (req, res) => {
+  try {
+    const { category, amount, source } = req.body;
+    const newExpense = new Expense({ category, amount, source, createdAt: Date.now() });
+    await newExpense.save();
+    res.status(201).json(newExpense);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.delete('/expenses/:id', async (req, res) => {
+  try {
+    const exps = await Expense.findByIdAndDelete(req.params.id);
+    res.status(200).json(exps);
+  } catch (err) {
+    res.status(401).json({ err })
+  }
+})
+
 
 export default router;
