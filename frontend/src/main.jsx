006f3eadd2ebfaed9ae8c4fe3bd1a8b3964962db
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-21 11:55:06 -0400

    add ProtectedRoute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
+import { AuthProvider } from './context/AuthContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
