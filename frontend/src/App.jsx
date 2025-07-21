51387f8e101aa1b21540566436b6ff95ca256cff
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-21 11:51:47 -0400

    additions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,47 @@
-// import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import './App.css'
+import ProtectedRoute from './ProtectedRoute.jsx';
+import { useAuth } from './context/AuthContext.jsx';
 
 function App() {
+  const { isAuthenticated, setIsAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLoginLogout = () => { 
+     setIsAuthenticated(false)
+   }
+
   return (
     <>
-    <nav className="bg-blue-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <a href='#' className="text-white text-lg font-semibold">
-Nuevo Tracker </a>
-         </div>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Login />} />
-       <Route path="/register" element={<Register />} />
-       <Route path="/dashboard" element={<Dashboard />} />
-   </Routes>
+      <nav className="bg-blue-800 p-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <a href='#' className="text-white text-lg font-semibold">Nuevo Tracker</a>
+          {isAuthenticated && <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleLoginLogout}>Logout</button>
+            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+          </div>}
+        </div>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+      </Routes>
     </>
   )
-  }
+}
 
 export default App
-   
