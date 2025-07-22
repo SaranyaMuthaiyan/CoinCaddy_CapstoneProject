f1d58cacd625c3134decce8e79749504922e39ca
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-22 11:12:47 -0400

    corrections

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import react from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
@@ -72,3 +72,4 @@ function Register() {
 }
 
 export default Register;
+
